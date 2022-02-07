ca689585bee3dc8c9d712bbbe29fdf2f2ec73e60
Simon Petrac <pfoerdie@gmail.com>
2022-02-07 10:11:12 +0100

    basic setup

---
+++ load.data.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    '@context':        'fua.load.rdf',
+    'dct:identifier':  path.join(__dirname, './load.data.js'),
+    'dct:format':      'application/fua.load+js',
+    'dct:title':       'load.data',
+    'dct:alternative': '@nrd/fua.resource.data',
+    'dct:requires':    []
+};
