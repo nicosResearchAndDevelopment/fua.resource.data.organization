08075271641f4521d6d1968d5db00a9ea00acf5e
Simon Petrac <pfoerdie@gmail.com>
2022-02-07 10:46:33 +0100

    added context

---
+++ context/context.default.js
@@ -0,0 +1,28 @@
+module.exports = {
+    // basic ontologies
+    'dc':   'http://purl.org/dc/elements/1.1/',
+    'dct':  'http://purl.org/dc/terms/',
+    'owl':  'http://www.w3.org/2002/07/owl#',
+    'rdf':  'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+    'xsd':  'http://www.w3.org/2001/XMLSchema#',
+
+    // common ontologies
+    'fno':  'https://w3id.org/function/ontology#',
+    'foaf': 'http://xmlns.com/foaf/0.1/',
+    'ldp':  'http://www.w3.org/ns/ldp#',
+    'odrl': 'http://www.w3.org/ns/odrl/2/',
+    'org':  'http://www.w3.org/ns/org#',
+    'skos': 'http://www.w3.org/2004/02/skos/core#',
+    'time': 'http://www.w3.org/2006/time#',
+    'vann': 'http://purl.org/vocab/vann/',
+    'vc':   'https://www.w3.org/2018/credentials/v1',
+    'voaf': 'http://purl.org/vocommons/voaf#',
+
+    // additional prefixes
+    'dbo': 'http://dbpedia.org/ontology/',
+    'dbr': 'http://dbpedia.org/resource/',
+
+    // custom prefixes
+    'fua': 'https://www.nicos-rd.com/fua#'
+};
--- data.test.js
+++ data.test.js
@@ -3,11 +3,18 @@ const
     expect           = require('expect'),
     {loadDataFiles}  = require('@nrd/fua.module.rdf'),
     {Dataset}        = require('@nrd/fua.module.persistence'),
+    res_context      = require('@nrd/fua.resource.data/context'),
     res_data         = require('@nrd/fua.resource.data');
 
-describe('resource.universe', function () {
+describe('resource.data', function () {
 
     this.timeout(0);
+
+    test('should have json context', async function () {
+        expect(typeof res_context).toBe('object');
+        expect(res_context.rdf).toBe('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+        expect(Object.values(res_context).every(val => typeof val === 'string')).toBeTruthy();
+    });
 
     test('should load universe with rdf-module', async function () {
         const dataFiles = await loadDataFiles(res_data);
